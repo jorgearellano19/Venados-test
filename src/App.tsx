bc35b0ad6af4f3073d6fd581e2cd2a49cbbf728e
Jorge Arturo Arellano del Aguila <jorch1910@gmail.com>
2020-03-17 21:30:51 -0700

    Style: Min-width added

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
+      minWidth: 360
     },
     appBar: {
       [theme.breakpoints.up('sm')]: {
